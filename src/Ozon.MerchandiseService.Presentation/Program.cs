451e47e5610a8a5e69f57c05eb34e9057d35d4f1
AlexSergo <alex_sergo@list.ru>
2021-10-21 19:03:45 +0300

    Fix name and docker-compose

--- src/Ozon.MerchandiseService.Presentation/Program.cs
+++ src/Ozon.MerchandiseService.Presentation/Program.cs
@@ -6,8 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ozon.MerchandiseService.Presentation;
 
-namespace merchandise_service
+namespace Ozon.MerchandiseService.Presentation
 {
     public class Program
     {
